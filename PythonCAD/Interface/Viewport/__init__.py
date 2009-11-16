5c5c2c1529b2a0fe4a4c89945d148f1c2c6582d0
matteoboscolo <euro_ii@libero.it>
2009-11-16 08:03:55 +0100

    New Structure for the R38 Branch

---
+++ PythonCAD/Interface/Viewport/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2002, Art Haas
+#
+# Copyright (c) 2009, Art Haas
+#
+# don't define an __all__ variable - the value of importing
+# different front-end interfaces into a module seems very
+# low right now ...
+#
+
