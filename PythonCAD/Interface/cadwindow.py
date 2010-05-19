fcb116eac930a3359ee7c6af21a7393094b2c59e
Gertwin <gertwin.groen@gmail.com>
2010-05-19 20:41:03 +0200

    Added Layer dockwindow

--- PythonCAD/Interface/cadwindow.py
+++ PythonCAD/Interface/cadwindow.py
@@ -33,6 +33,7 @@ from Interface.cadscene import CadScene
 from Interface.cadview import CadView
 
 from Interface.CmdIntf.cmdintf import CmdIntf
+from Interface.LayerIntf.layerdock import LayerDock
 
 
 class CadWindow(QtGui.QMainWindow):
@@ -151,18 +152,8 @@ class CadWindow(QtGui.QMainWindow):
         Creates all dockable windows for the application
         '''
         # layer list
-        layer_dock = QtGui.QDockWidget("Layers", self)
-        layer_dock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
-        self.__layer_list = QtGui.QListWidget(layer_dock)
-        self.__layer_list.addItems((
-            "Layer 0",
-            "Just an layer name",
-            "Another layer",
-            "The last layer"))
-        
-        layer_dock.setWidget(self.__layer_list)
+        layer_dock = LayerDock(self)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, layer_dock)
-        #self.viewMenu.addAction(layer_dock.toggleViewAction())
         
         # commandline
         command_dock = self.__cmd_intf.Commandline
