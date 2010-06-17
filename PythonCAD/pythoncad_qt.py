2e05943fb824d09089e3738ab70113bd736c0730
Matteo Boscolo <matteo.boscolo@boscolini.eu>
2010-06-17 08:51:42 +0200

    some fix on the open drawing Remove mdi file ..

--- PythonCAD/pythoncad_qt.py
+++ PythonCAD/pythoncad_qt.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 # This is only needed for Python v2 but is harmless for Python v3.
@@ -12,19 +13,14 @@ import sqlite3 as sqlite
 # this is needed for me to use unpickle objects
 sys.path.append(os.path.join(os.getcwd(), 'Generic'))
 
-from Generic.application import Application
-from Interface.cadwindow import CadWindow
-from Interface.pycadapp import PyCadApp
+from Generic.application        import Application
+from Interface.cadwindow        import CadWindowMdi
+
 
 if __name__ == '__main__':
     import sys
     app = QtGui.QApplication(sys.argv)
-    # global_cad_window is from globals
-    PyCadApp.SetApplication(Application())
-    # global_cad_window is from globals
-    PyCadApp.SetCadWindow(CadWindow())
-    # show the main window and enter event loop
-    PyCadApp.CadWindow().show()
+    w=CadWindowMdi()
+    w.show()
     sys.exit(app.exec_())
-    
     
