cd07db6952a0d7b11064de26db4c7344bb64dd72
Matteo Boscolo <matteo.boscolo@boscolini.eu>
2010-12-03 07:47:16 +0100

    py installer script modification

--- pyInstaller.bat
+++ pyInstaller.bat
@@ -1,4 +1,4 @@
 rem _PyInstaller
-cd C:\Users\mboscolo\Desktop\R38\trunk
+cd C:\Users\mboscolo\Desktop\R38\pyinstaller-trunk
 python Makespec.py -n PythonCAD --icon C:\Users\mboscolo\Desktop\R38\pythoncad\PythonCAD\icons\pythoncad.ico -p C:\Users\mboscolo\Desktop\R38\pythoncad\PythonCAD;C:\Users\mboscolo\Desktop\R38\pythoncad\PythonCAD\Generic;C:\Users\mboscolo\Desktop\R38\pythoncad\PythonCAD\Generic\Kernel;C:\Users\mboscolo\Desktop\R38\pythoncad\PythonCAD\Interface C:\Users\mboscolo\Desktop\R38\pythoncad\PythonCAD\pythoncad_qt.py
-python Build.py C:\Users\mboscolo\Desktop\R38\trunk\pythoncad_qt\pythoncad_qt.spec
+python Build.py C:\Users\mboscolo\Desktop\R38\pyinstaller-trunk\pythoncad\PythonCAD.spec
